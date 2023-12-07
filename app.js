b0963e9a9e62699f3759d3cd8ad1e715ff231c72
zaheerkhan1122 <147386260+zaheerkhan1122@users.noreply.github.com>
2023-12-08 00:41:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", function () {
+//   let h3 = document.querySelector("h3");
+//   let randomColor = getRandomColor();
+//   h3.innerText = randomColor;
+//   let div = document.querySelector("div");
+//   div.style.backgroundColor = randomColor;
+// });
+
+// function getRandomColor() {
+//   let red = Math.floor(Math.random() * 255);
+//   let green = Math.floor(Math.random() * 255);
+//   let blue = Math.floor(Math.random() * 255);
+//   let color = `rgb(${red}, ${green}, ${blue})`;
+//   return color;
+// }
+
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", function() {
+    let h4 = document.querySelector("h4");
+    let randomColor = getRandomColor();
+    h4.innerText = randomColor;
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+});
+
+
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
